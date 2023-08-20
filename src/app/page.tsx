28fe474b79ffdb267746c4a4dc2c2bfd54f03ac8
Vincenzo M <vincenzomaiorana@gmail.com>
2023-08-20 12:19:15 +0200

    main layout add player

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
+import RadioPlayer from "./components/RadioPlayer";
 import "./page.module.scss";
 
 export default function Home() {
   return (
     <>
-      <main>Website compiles!</main>
+      <main className="container">
+        <div className="row">
+          <RadioPlayer />
+        </div>
+      </main>
     </>
   );
 }
