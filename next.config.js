cacddece83d649a6e34ff52bb5048c1639dc0d92
Vincenzo M <vincenzomaiorana@gmail.com>
2023-08-20 12:18:25 +0200

    add import for radiosplash domain

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+   images: {
+      remotePatterns: [
+         {
+            protocol: 'https',
+            hostname: 'www.radiosplash.it',
+            port: '',
+            pathname: '/**',
+         },
+         {
+            protocol: 'https',
+            hostname: 'upload.radiosplash.it',
+            port: '',
+            pathname: '/**',
+         },
+      ],
+   },
+}
 
 module.exports = nextConfig
