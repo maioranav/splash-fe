4eae233d1ceeee2fe5a1a60cb6f5b19ede74cf53
Vincenzo M <vincenzomaiorana@gmail.com>
2023-08-25 23:39:00 +0200

    fix real stream video, fix user-select onmouse

--- src/app/components/CustomFooter/CustomFooter.tsx
+++ src/app/components/CustomFooter/CustomFooter.tsx
@@ -1,3 +1,22 @@
+"use client";
 export default function CustomFooter() {
-  return <>Footer Content</>;
+  return (
+    <div className="bg-primary text-light py-4">
+      <div className="container">
+        <div className="row">
+          <div className="col" style={{ fontSize: "0.5rem" }}>
+            Radio Splash © {new Date().getFullYear()} | Tutti i diritti riservati | Safer S.R.L. | Partita Iva: 02055490839 | Powered by
+            <a href="https://www.vincenzomaiorana.it" target="_blank" className="text-light text-decoration-none">
+              &nbsp;Vincenzo Maiorana&nbsp;
+            </a>
+            and
+            <a href="https://www.vincenzocarcione.com" target="_blank" className="text-light text-decoration-none">
+              &nbsp;Vincenzo Carcione&nbsp;
+            </a>
+            | Cookie Policy
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
