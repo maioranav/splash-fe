84d13f22e215e397cc3db35744b9e191856dbac0
Vincenzo <vincenzomaiorana@gmail.com>
2023-09-12 21:37:15 +0200

    implement data from json in palinsesto

--- src/app/components/Calendar/Calendar.tsx
+++ src/app/components/Calendar/Calendar.tsx
@@ -1,17 +1,21 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./Calendar.scss";
+import bepalinsesto from "../../../../public/palinsesto.json";
+
 export const Calendar = () => {
+  const [hasTableBeenGenerated, setTableBeenGenerated] = useState(false);
+
   const generateTableStructure = (hoursOfDay: number, daysOfWeek: number) => {
     let hours = "";
     for (let i = 0; i < hoursOfDay * 2; i++) {
-      hours += "<tr>";
       const isEven = i % 2 == 0;
-      let hoursHead = isEven ? `<th className="headcol">${i / 2}.00</th>` : `<th className="headcol"></th>`;
+      hours += `<tr id="h${isEven ? i / 2 : ""}">`;
+      let hoursHead = isEven ? `<th className="headcol" >${i / 2}.00</th>` : `<th className="headcol"></th>`;
       hours += hoursHead;
       let days = "";
-      for (let e = 0; e <= daysOfWeek; e++) {
-        days += "<td></td>";
+      for (let e = 0; e < daysOfWeek; e++) {
+        days += `<td class="d${e}"></td>`;
       }
       hours += days;
       hours += "</tr>";
@@ -19,18 +23,51 @@ export const Calendar = () => {
     return hours;
   };
 
+  const generateContent = async () => {
+    const palinsesto = bepalinsesto.palinsesto;
+    for (let d = 0; d < palinsesto.length; d++) {
+      for (let el = 0; el < palinsesto[d].length; el++) {
+        let startTime = parseInt(palinsesto[d][el].start.split(".")[0]);
+        if (parseInt(palinsesto[d][el].start.split(".")[1]) > 0) startTime += 0.5;
+        let endTime = parseInt(palinsesto[d][el].end.split(".")[0]);
+        if (parseInt(palinsesto[d][el].end.split(".")[1]) > 0) endTime += 0.5;
+        if (endTime == 0) endTime = 24;
+        const duration = endTime - startTime;
+        const height = duration * 220;
+        const branch = document.querySelector(`.bodyCalendar #h${startTime} .d${d}`);
+        if (branch) {
+          let toInsert = `<div class='event' style="height: ${height}%;`;
+          toInsert += startTime % 1 != 0 ? ` transform: translateY(100%);` : ``;
+          toInsert += `"><p class="event-title">${palinsesto[d][el].titolo}</p>`;
+          if (palinsesto[d][el].artista) toInsert += `<p class="event-artist">${palinsesto[d][el].artista}</p>`;
+          toInsert += `<p class="event-time">${palinsesto[d][el].start} - ${palinsesto[d][el].end}</p></div>`;
+          branch.innerHTML = toInsert;
+        }
+      }
+    }
+  };
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
     if (globalThis.document) {
       let bodyCalendar = globalThis.document.querySelector(".bodyCalendar");
-      if (bodyCalendar) bodyCalendar.innerHTML = generateTableStructure(24, 7);
+      if (bodyCalendar) {
+        bodyCalendar.innerHTML = generateTableStructure(24, 7);
+        setTableBeenGenerated(true);
+      }
     }
   });
 
+  useEffect(() => {
+    if (!hasTableBeenGenerated) return;
+    generateContent();
+  }, [hasTableBeenGenerated]);
+
   return (
-    <>
-      <div className="calendar">
-        <div className="outer">
-          <table>
+    <div className="calendar">
+      <div className="outer">
+        <div className="wrap">
+          <table className="offset">
             <thead>
               <tr>
                 <th className="headcol"></th>
@@ -43,15 +80,10 @@ export const Calendar = () => {
                 <th>Domenica</th>
               </tr>
             </thead>
+            <tbody className="bodyCalendar">Loading...</tbody>
           </table>
-
-          <div className="wrap">
-            <table className="offset">
-              <tbody className="bodyCalendar">Loading...</tbody>
-            </table>
-          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
