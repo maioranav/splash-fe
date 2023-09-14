dbfae157997b3bcf6a3de7354ad1282fd04e5a03
Vincenzo <vincenzomaiorana@gmail.com>
2023-09-14 23:05:04 +0200

    css layout palinsesto mobile

--- src/app/palinsesto/page.tsx
+++ src/app/palinsesto/page.tsx
@@ -19,17 +19,19 @@ export default function Palinsesto() {
             <div className="row my-4 d-none d-md-block">
               <Calendar data={palinsesto.palinsesto} />
             </div>
-            <div className="row my-4 d-block d-md-none">
+            <div className="row my-4 d-block d-md-none palinsesto-mobile">
               <h2 className="fw-bold">Palinsesto</h2>
               {palinsesto.palinsesto.map((el, i) => (
                 <>
-                  <div key={"day-" + i}>Giorno {i}</div>
-                  <ul>
+                  <div key={"day-" + i} className="giorno-settimana">
+                    Giorno {i}
+                  </div>
+                  <ul className="settimana">
                     {el.map((p, pi) => (
-                      <li key={"prog-" + pi + "-wd-" + i}>
-                        <div>{p.titolo}</div>
-                        {p.artista && <div>{p.artista}</div>}
-                        <div>
+                      <li key={"prog-" + pi + "-wd-" + i} className="programma">
+                        <div className="titolo">{p.titolo}</div>
+                        {p.artista && <div className="artista">{p.artista}</div>}
+                        <div className="orario">
                           {p.start} - {p.end}
                         </div>
                       </li>
