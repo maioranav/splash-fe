825573b6096d9e5eb6aace6d8ae926c428ed5952
Vincenzo <vincenzomaiorana@gmail.com>
2024-12-27 19:47:34 +0100

    fix(footer): position fixed

--- src/app/components/footer/CustomFooter.tsx
+++ src/app/components/footer/CustomFooter.tsx
@@ -2,7 +2,7 @@ export const CustomFooter = () => {
   const year = new Date().getFullYear();
 
   return (
-    <div className="row position-absolute bottom-0 m-0 p-0 w-100 d-flex flex-wrap">
+    <div className="row position-fixed bottom-0 m-0 p-0 w-100 d-flex flex-wrap">
       <div className="col-12 col-md-7 bg-secondary d-flex justify-content-end align-items-center py-3">
         <p className="m-0 p-0 text-light small">Radio Splash &copy; {year} | Tutti i diritti riservati | Safer Srl | P.IVA: 02055490839</p>
       </div>
