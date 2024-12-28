5cc3ed2483dcfdbaa0a4e7cca029823cc1a908ed
Vincenzo <vincenzomaiorana@gmail.com>
2024-12-28 12:41:03 +0100

    feat(tv): add video player for livetv

--- src/app/tv/page.tsx
+++ src/app/tv/page.tsx
@@ -1,3 +1,23 @@
-export default async function Tv() {
-  return <>Pagina da implementare</>;
+import { VideoPlayer } from "../components/videoplayer/VideoPlayer";
+
+export default function Tv() {
+  const videoJsOptions = {
+    autoplay: false,
+    controls: true,
+    responsive: true,
+    poster: "/imgs/splashtvstart.jpg",
+    fluid: true,
+    sources: [
+      {
+        src: process.env.NEXT_PUBLIC_BASE_VIDEO_STREAM,
+        type: "application/vnd.apple.mpegurl",
+      },
+    ],
+  };
+
+  return (
+    <div className="container mt-5">
+      <VideoPlayer options={videoJsOptions} />
+    </div>
+  );
 }
